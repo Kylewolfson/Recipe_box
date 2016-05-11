f95ce018ca46a4144c0286b09c9278fa45e60890
Anthony Gold and Kyle Wolfson <anthony@>
2016-05-11 14:11:14 -0700

    create root template in App.java, build out front page in index.vtl

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,15 +7,16 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
-    // staticFileLocation("/public");
-    // String layout = "templates/layout.vtl";
-    //
-    // get("/", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/index.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      model.put("tags", Tag.all());
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
     // get("/categories", (request, response) -> {
     //   HashMap<String, Object> model = new HashMap<String, Object>();
     //   model.put("categories", Category.all());
